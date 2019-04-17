600fc85bb75c524a2256a3ad8fc993f8628904ec
M B <mmb8830@g.rit.edu>
2019-04-17 18:41:54 -0400

    First commit with news feed functionality and page layout

---
+++ main.js
@@ -0,0 +1,41 @@
+//this forces javascript to conform to some rules, like declaring variables with var
+"use strict";
+$(document).ready(function () {
+  var urls = ["http://www.espn.com/espn/rss/news",
+              "http://www.espn.com/espn/rss/nfl/news",
+              "http://www.espn.com/espn/rss/nba/news",
+              "http://www.espn.com/espn/rss/mlb/news",
+              "http://www.espn.com/espn/rss/nhl/news",
+              "http://www.espn.com/espn/rss/golf/news",
+              "http://www.espn.com/espn/rss/rpm/news"];
+    for (var i = 0; i < urls.length; i++) {
+        request(urls[i], i);
+    }
+});
+function request(urlToRequest, contentNum) {
+    $.ajax({
+        url: urlToRequest,
+        success: function (data) {
+          var items = data.querySelectorAll("item");
+          //parse the data
+          var html = "";
+          for (var i=0; i<items.length; i++) {
+            //get the data out of the item
+            var newsItem = items[i];
+            var title = newsItem.querySelector("title").firstChild.nodeValue;
+            var description = newsItem.querySelector("description").firstChild.nodeValue;
+            var link = newsItem.querySelector("link").firstChild.nodeValue;
+            var pubDate = newsItem.querySelector("pubDate").firstChild.nodeValue;
+
+            //present the item as HTML
+            var line = '<div class="card"><div class="card-body">';
+            line += '<h5 class="card-title">'+title+"</h5>";
+            line += '<p class="card-text"><i>'+pubDate+'</i> - <a href="'+link+'" target="_blank" class="btn btn-secondary">See original</a></p>';
+            line += "</div></div>";
+            html += line;
+          }
+          console.log(`URL: ${urlToRequest}, contentNum: ${contentNum}`);
+          document.querySelector(`#content${contentNum}`).innerHTML = html;
+        }
+    });
+}
